f8a277a16db241d58a5e2bd86bd0cfe8ac7f0a7c
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2021-05-07 10:46:47 +0200

    Use the correct cut on the jet collection
    Fixes #7

--- tasks/4/rdataframe_compiled.cxx
+++ tasks/4/rdataframe_compiled.cxx
@@ -5,9 +5,9 @@ void rdataframe() {
     ROOT::EnableImplicitMT();
     ROOT::RDataFrame df("Events", "root://eospublic.cern.ch//eos/root-eos/benchmark/Run2012B_SingleMu.root");
     auto filter = [](const ROOT::RVec<float> & pt, const ROOT::RVec<float> & eta) {
-            return Sum(pt > 40 && abs(eta) < 1.0) > 1;
+            return Sum(pt > 40) > 1;
     };
-    auto h = df.Filter(filter, {"Jet_pt", "Jet_eta"}, "More than one jet with pt > 40 and abs(eta) < 1.0")
+    auto h = df.Filter(filter, {"Jet_pt", "Jet_eta"}, "More than one jet with pt > 40")
                .Histo1D<float>({"", ";MET (GeV);N_{Events}", 100, 0, 2000}, "MET_pt");
 
     TCanvas c;
