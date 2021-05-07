da4bf4a965ca949722be99e421d55d6b50a5b419
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2021-05-07 10:39:33 +0200

    Use MET_pt instead of MET_sumet
    Fixes #5

--- tasks/1/rdataframe_jitted.C
+++ tasks/1/rdataframe_jitted.C
@@ -1,7 +1,7 @@
 void rdataframe_jitted() {
     ROOT::EnableImplicitMT();
     ROOT::RDataFrame df("Events", "root://eospublic.cern.ch//eos/root-eos/benchmark/Run2012B_SingleMu.root");
-    auto h = df.Histo1D({"", ";MET (GeV);N_{Events}", 100, 0, 2000}, "MET_sumet");
+    auto h = df.Histo1D({"", ";MET (GeV);N_{Events}", 100, 0, 2000}, "MET_pt");
 
     TCanvas c;
     h->Draw();
--- tasks/4/rdataframe_compiled.cxx
+++ tasks/4/rdataframe_compiled.cxx
@@ -8,7 +8,7 @@ void rdataframe() {
             return Sum(pt > 40 && abs(eta) < 1.0) > 1;
     };
     auto h = df.Filter(filter, {"Jet_pt", "Jet_eta"}, "More than one jet with pt > 40 and abs(eta) < 1.0")
-               .Histo1D<float>({"", ";MET (GeV);N_{Events}", 100, 0, 2000}, "MET_sumet");
+               .Histo1D<float>({"", ";MET (GeV);N_{Events}", 100, 0, 2000}, "MET_pt");
 
     TCanvas c;
     h->Draw();
