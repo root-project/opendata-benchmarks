ace9469d5788133044ba0d888ec370a6149478b5
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2021-05-07 10:42:33 +0200

    Correct eta cut
    Fixes #6

--- tasks/3/rdataframe_compiled.cxx
+++ tasks/3/rdataframe_compiled.cxx
@@ -4,7 +4,7 @@
 void rdataframe() {
     ROOT::EnableImplicitMT();
     ROOT::RDataFrame df("Events", "root://eospublic.cern.ch//eos/root-eos/benchmark/Run2012B_SingleMu.root");
-    auto goodJetPt = [](const ROOT::RVec<float> &pt, const ROOT::RVec<float> &eta) { return pt[eta > 1.0]; };
+    auto goodJetPt = [](const ROOT::RVec<float> &pt, const ROOT::RVec<float> &eta) { return pt[abs(eta) < 1.0]; };
     auto h = df.Define("goodJet_pt", goodJetPt, {"Jet_pt", "Jet_eta"})
                .Histo1D<ROOT::RVec<float>>({"", ";Jet p_{T} (GeV);N_{Events}", 100, 15, 60}, "goodJet_pt");
 
