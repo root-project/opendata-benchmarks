6752b47fef2f91b337ba479a462b6af62b3da2ce
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2021-05-07 10:42:33 +0200

    Correct eta cut
    Fixes #6

--- tasks/3/rdataframe_jitted.C
+++ tasks/3/rdataframe_jitted.C
@@ -1,7 +1,7 @@
 void rdataframe_jitted() {
     ROOT::EnableImplicitMT();
     ROOT::RDataFrame df("Events", "root://eospublic.cern.ch//eos/root-eos/benchmark/Run2012B_SingleMu.root");
-    auto h = df.Define("goodJet_pt", "Jet_pt[Jet_eta > 1.0]")
+    auto h = df.Define("goodJet_pt", "Jet_pt[abs(Jet_eta) < 1.0]")
                .Histo1D({"", ";Jet p_{T} (GeV);N_{Events}", 100, 15, 60}, "goodJet_pt");
 
     TCanvas c;
